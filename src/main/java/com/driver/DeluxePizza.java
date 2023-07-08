28a3c104202fac927e739c9646f4da3c1e5f48e6
prakharsaki <prakhar2614@gmail.com>
2023-07-08 23:23:08 +0530

    Added code to methods

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -4,6 +4,8 @@ public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        setFlagForCheese(true);
+        setFlagForToppings(true);
         // your code goes here
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,32 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
+    private boolean flagForCheese=false;
+    private boolean flagForToppings=false;
     private String bill;
+
+    public boolean isFlagForCheese() {
+        return flagForCheese;
+    }
+
+    public void setFlagForCheese(boolean flagForCheese) {
+        this.flagForCheese = flagForCheese;
+    }
+
+    public boolean isFlagForToppings() {
+        return flagForToppings;
+    }
+
+    public void setFlagForToppings(boolean flagForToppings) {
+        this.flagForToppings = flagForToppings;
+    }
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)price=300;
+        else price=400;
+
     }
 
     public int getPrice(){
@@ -17,18 +38,27 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(flagForCheese)return;
+        price+=80;
+        flagForCheese=true;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(flagForToppings)return;
+        if(isVeg)price+=70;
+        else price+=120;
+        flagForToppings=true;
     }
 
     public void addTakeaway(){
         // your code goes here
+        price+=20;
     }
 
     public String getBill(){
         // your code goes here
+        bill=String.valueOf(price);
         return this.bill;
     }
 }
